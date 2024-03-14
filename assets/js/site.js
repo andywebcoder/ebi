9513657a65ae7dd557fa3a3aa64c6364159a1721
andy <andywebcoder@gmail.com>
2024-03-15 03:36:09 +0900

    add

--- assets/js/site.js
+++ assets/js/site.js
@@ -9,17 +9,6 @@ $(function($) {
 
 function fadeAnime(){
 // 回転
-$('.rotateLeftZTrigger').each(function(){
-    var elemPos = $(this).offset().top-50;
-    var scroll = $(window).scrollTop();
-    var windowHeight = $(window).height();
-    if (scroll >= elemPos - windowHeight){
-        $(this).addClass('rotateLeftZ');
-    }else{
-        $(this).removeClass('rotateLeftZ');
-    }
-});
-    
 $('.rotateRightZTrigger').each(function(){
     var elemPos = $(this).offset().top-50;
     var scroll = $(window).scrollTop();
