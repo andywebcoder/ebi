f824b82f60129bb1abc4152a9a075ce1578f9748
andy <andywebcoder@gmail.com>
2024-03-15 03:05:14 +0900

    add

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,48 +1,41 @@
-//headerの読み込み・PC時お問い合わせモーダル用
-// fetch("/partial/header.html")
-//     .then((response) => response.text())
-//     .then((data) => document.querySelector("#js-header").innerHTML = data);
-// 
-// //footerの読み込み・スマホ時MENUモーダル用
-// fetch("/partial/footer.html")
-//     .then((response) => response.text())
-//     .then((data) => document.querySelector("#js-footer").innerHTML = data);
-
-
 $(function($) {
-    $('.header-logo').on('click',function(){
+    $('.header-logo, .top-logo_sp, .header-hamburger_line').on('click',function(){
         $('.header-hamburger_line').toggleClass('active');
         $('.header-nav').toggleClass('active');
         $('body').toggleClass('is-open');
         $('.js-nav').toggleClass('is-open');
     });
-    $('.header-hamburger_line').on('click',function(){
-        // $('.header-nav').toggleClass('active');
-    })
 });
 
-$(function () {
-    // スクロール時の処理
-    $(window).scroll(function () {
-      // 対象要素に初期状態のスタイルを付与
-    $(".scroll-anim").addClass("ready");
-
-    // ウィンドウの高さを取得
-    const windowHeight = $(this).height();
-
-    // 現在の座標を取得
-    const scrollAmount = $(this).scrollTop();
-
-    $(".ready").each(function () {
-    // 対象要素の座標を取得
-    const targetPosition = $(this).offset().top;
-
-    // 対象要素が画面内に入った時の処理
-    if (scrollAmount > targetPosition - windowHeight + 60) {
-        $(this).addClass("active");
-    } else {
-        $(this).removeClass("active");
+function fadeAnime(){
+// 回転
+$('.rotateLeftZTrigger').each(function(){
+    var elemPos = $(this).offset().top-50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+        $(this).addClass('rotateLeftZ');
+    }else{
+        $(this).removeClass('rotateLeftZ');
     }
-    });
 });
+    
+$('.rotateRightZTrigger').each(function(){
+    var elemPos = $(this).offset().top-50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight)
+    {
+        $(this).addClass('rotateRightZ');
+    }else{
+        $(this).removeClass('rotateRightZ');
+    }
+});
+}
+
+$(window).scroll(function (){
+    fadeAnime();
+});
+$(window).on('load', function(){
+    fadeAnime();
 });
