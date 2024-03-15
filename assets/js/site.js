dc5639d6204d6bd13d12f9a232fa5a27db250780
andy <andywebcoder@gmail.com>
2024-03-16 02:31:11 +0900

    add

--- assets/js/site.js
+++ assets/js/site.js
@@ -7,24 +7,31 @@ $(function($) {
     });
 });
 
-function fadeAnime(){
-// 回転
-$('.rotateRightZTrigger').each(function(){
-    var elemPos = $(this).offset().top-50;
-    var scroll = $(window).scrollTop();
-    var windowHeight = $(window).height();
-    if (scroll >= elemPos - windowHeight)
-    {
-        $(this).addClass('rotateRightZ');
-    }else{
-        $(this).removeClass('rotateRightZ');
-    }
-});
-}
+// function fadeAnime(){
+// // 回転
+// $('.rotateRightZTrigger').each(function(){
+//     var elemPos = $(this).offset().top-50;
+//     var scroll = $(window).scrollTop();
+//     var windowHeight = $(window).height();
+//     if (scroll >= elemPos - windowHeight)
+//     {
+//         $(this).addClass('rotateRightZ');
+//     }else{
+//         $(this).removeClass('rotateRightZ');
+//     }
+// });
+// }
+// 
+// $(window).scroll(function (){
+//     fadeAnime();
+// });
+// $(window).on('load', function(){
+//     fadeAnime();
+// });
 
-$(window).scroll(function (){
-    fadeAnime();
-});
-$(window).on('load', function(){
-    fadeAnime();
+// スクロールして表示領域に入ったらclass付与
+$(function () {
+    $(".js-rotate,.js-popup,.js-content-sp,.js-popup_img1").on("inview", function () {
+        $(this).addClass("is-inview");
+    });
 });
